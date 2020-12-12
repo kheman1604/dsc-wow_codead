a41828ba929e5c1fc14acd197c50061a194d1691
kheman1604 <official.kheman@gmail.com>
2020-12-12 12:12:45 +0530

    First Commit

---
+++ models/job.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+const Applicant = require("./applicant");
+ 
+var jobSchema = new mongoose.Schema({
+   jname: String,
+   gender:String,
+   salary:String,
+   location:String,
+   pincode:String,
+   email:String,
+   description:String,
+   userid:String,
+   jtype:String,
+   tenure:String,
+   timing:String,
+   applicants: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Applicant"
+      }
+   ]
+});
+ 
+module.exports = mongoose.model("Job", jobSchema);
