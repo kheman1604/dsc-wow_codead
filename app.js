1e99d1725e1c4193a8dff2b9a656fd76d020cf0d
abhipadhy <abhishekpadhyakr@gmail.com>
2020-12-12 14:24:22 +0530

    app.js chnges

--- app.js
+++ app.js
@@ -25,3 +25,78 @@ var transporter = nodemailer.createTransport({
 	  pass: npass
 	}
   });
+  mongoose.connect(uri, { useNewUrlParser: true , useUnifiedTopology: true })
+  .then(() => 
+  console.log("Database is connected!"))
+  
+  .catch((err) => console.error(err))
+  
+  app.use(express.static(path.join(__dirname, '/public')));
+  //passport config
+  app.use(require("express-session")({
+      secret:ss,
+      resave:false,
+      saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new LocalStrategy(User.authenticate()) );
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+  
+  app.use(bodyparser.urlencoded({extended:true}));
+  var port=process.env.PORT || 3500;
+  app.listen(port,process.env.IP,function(){
+      console.log("server started.");
+  });
+  
+  app.get("/",function(req,res){
+      res.render("index.ejs",{currentUser:req.user});
+  });
+  app.get("/search",function(req,res){
+      Job.find({},function(err,alljobs){
+          if(err)
+              console.log(err);
+          else
+              res.render("search.ejs",{jobs:alljobs,currentUser:req.user});
+      });
+     
+  });
+  app.get('/register',function(req,res){
+      res.render("register.ejs",{currentUser:req.user});
+  });
+  app.get('/dash',function(req,res){
+      Job.find({userid:req.user._id},function(err,jobs){
+          if(err)
+          console.log(err)
+          else
+          {
+              res.render("dash.ejs",{currentUser:req.user,jobs:jobs});
+          }
+          
+  
+      })
+      
+  });
+  
+  app.get('/delete',function(req,res){
+      Job.find({userid:req.user._id},function(err,jobs){
+          if(err)
+          console.log(err)
+          else
+          {
+              res.render("delete.ejs",{currentUser:req.user,jobs:jobs});
+          }
+          
+  
+      })
+  });
+  
+  app.get('/dash/:id/deleted',function(req,res){
+          Job.remove({_id:req.params.id},function(err,result){
+              res.render('deleted.ejs',{currentUser:req.user});
+          });
+          
+      });
+  
+  
